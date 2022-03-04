8fe1bd8d3dffb07431c79f176c67a48a150f2470
World <77151113+World-Tan@users.noreply.github.com>
2022-03-05 01:48:31 +0200

    Update SlaveArduino.cpp

--- SlaveArduino.cpp
+++ SlaveArduino.cpp
@@ -1,1 +1,18 @@
+/*	libraries	*/
 
+#include <Wire.h>
+
+
+/*	constants	*/
+
+// I2C registers' addresses
+const int addressMaster = b1; // The address of the master's register.
+
+
+
+/*	main code	*/
+
+void setup() {
+	// I2C shit
+	wire.begin(b1000101); // turn on the I2C and set the address of this device's register.
+}
